db91a76f74f8ed5d84e0acc281804bff2cd6606d
Nathan Birch <birchn@byui.edu>
2022-03-16 16:07:40 -0600

    cors fix

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const cors = require('cors');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
+  .use(
+    cors({
+      origin: '*'
+    })
+  )
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
