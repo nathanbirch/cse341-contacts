e6280a06af936525a1dce2daf51ea0e90d255c37
Nathan Birch <birchn@byui.edu>
2022-03-18 14:00:04 -0600

    fixing swagger docs

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 router.use('/api-docs', swaggerUi.serve);
-router.use('/api-docs', swaggerUi.setup(swaggerDocument));
+router.use(
+  '/api-docs',
+  swaggerUi.setup(swaggerDocument, {
+    explorer: true
+  })
+);
 
 module.exports = router;
