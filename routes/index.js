a1ad1b0ef85271b204104f32e4f55f866cac0c01
Nathan Birch <birchn@byui.edu>
2023-09-15 11:42:59 -0600

    adding lesson 1 route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/', (req, res) => {
+  res.send('Sarah Birch');
+});
+
 router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'));
 
